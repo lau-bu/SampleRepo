c57ce2a1dc7e05c2cd08ed1cee4fd7f128a6ffc7
Siu-Cheong Lau <scllouis@gmail.com>
2026-06-11 10:42:47 -0400

    minor change

--- scripts/add_numbers.py
+++ scripts/add_numbers.py
@@ -2,10 +2,8 @@
 
 import argparse
 
-
 def add_numbers(a: float, b: float) -> float:
     return a + b
-
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Add two numbers.")
@@ -13,7 +11,6 @@ def main() -> None:
     parser.add_argument("b", type=float, help="Second number")
     args = parser.parse_args()
     print(add_numbers(args.a, args.b))
-
 
 if __name__ == "__main__":
     main()
